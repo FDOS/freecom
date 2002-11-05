717e169761392b3a0b42dc1e1dc8bae37e25288e
Steffen Kaiser <Steffen.Kaiser@fh-rhein-sieg.de>
2002-11-05 19:27:37 +0000

    bugfix: FreeCOM is to pass the fully-qualified path to DOS-4B-00
    git-svn-id: https://svn.code.sf.net/p/freedos/svn/freecom/trunk@451 6ac86273-5f31-0410-b378-82cca8765d1b

--- lib/where.c
+++ lib/where.c
@@ -7,6 +7,9 @@
 	This file bases on OPENF.C of FreeCOM v0.81 beta 1.
 
 	$Log$
+	Revision 1.2  2002/11/05 19:27:37  skaus
+	bugfix: FreeCOM is to pass the fully-qualified path to DOS-4B-00
+
 	Revision 1.1  2001/04/12 00:33:53  skaus
 	chg: new structure
 	chg: If DEBUG enabled, no available commands are displayed on startup
@@ -30,7 +33,7 @@
 	chg: splitted code apart into LIB\*.c and CMD\*.c
 	bugfix: IF is now using error system & STRINGS to report errors
 	add: CALL: /N
-
+	
  */
 
 #include "../config.h"
@@ -43,9 +46,14 @@
 #include "../include/misc.h"
 
 char *find_which(const char * const fname)
-{
+{	char *p;
   static char *buf = 0;
 
+	if(0 == (p = dfnsearch(fname, 0, 0)))
+		return 0;
+
   free(buf);
-  return buf = dfnsearch(fname, 0, 0);
+  buf = dfnfullpath(p);
+  free(p);
+  return buf;
 }
