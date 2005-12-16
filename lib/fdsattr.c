c01c81b13b9e5a7cc596d122adb9dc1bd6e96017
Kenneth J Davis <PerditionC@gmail.com>
2005-12-16 12:03:39 +0000

    use proper subfunction to set attributes (from Eric)
    git-svn-id: https://svn.code.sf.net/p/freedos/svn/freecom/trunk@1172 6ac86273-5f31-0410-b378-82cca8765d1b

--- lib/fdsattr.c
+++ lib/fdsattr.c
@@ -7,9 +7,12 @@
  *    else: DOS error code
 
 	$Log$
+	Revision 1.3  2005/12/16 12:03:39  perditionc
+	use proper subfunction to set attributes (from Eric)
+
 	Revision 1.2  2004/02/01 13:52:17  skaus
 	add/upd: CVS $id$ keywords to/of files
-
+	
 	Revision 1.1  2002/04/08 16:02:22  skaus
 	bugfix: CTTY: added to try to set the device flags
 	
@@ -21,7 +24,7 @@ int fdsetattr(const int fd, const int attr)
 {
   union REGS r;
 
-  r.x.ax = 0x4400;              /* Get handle information */
+  r.x.ax = 0x4401;              /* Get handle information */
   r.x.bx = fd;
   r.x.dx = attr;
   int86(0x21, &r, &r);
