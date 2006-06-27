ffed531f09fd825a9737b17e1733ad9375d39940
Blair Campbell <blairdude@gmail.com>
2006-06-27 02:33:47 +0000

    2nd attempt at getting a working 2e handler (still not working)
    git-svn-id: https://svn.code.sf.net/p/freedos/svn/freecom/trunk@1232 6ac86273-5f31-0410-b378-82cca8765d1b

--- shell/xms_2e.asm
+++ shell/xms_2e.asm
@@ -6,59 +6,70 @@
 segment _TEXT class=CODE
 
 extern _residentCS, _mySS, _mySP, _XMSsave, _XMSdriverAdress, _SwapTransientSize, _my2e_parsecommandline, SWAPXMSdirection, _SwapResidentSize
-%define callXMS     call far [_XMSdriverAdress]
+%define callXMS     call far [cs:_XMSdriverAdress]
 %define currentSegmOfFreeCOM    _XMSsave+8
+
+    global myfar2e_parsecommandline
+myfar2e_parsecommandline:
+    call _my2e_parsecommandline
+    retf
 
 	global _lowlevel_int_2e_handler
 _lowlevel_int_2e_handler:
-;       push ss
-;       push sp
-;       push cs
+        cld
+        push si
 ;       lss sp,[_mySP]
-;       mov ss,[_mySS]
-;       mov sp,[_mySP]
+        mov ss,[cs:_mySS]
+        mov sp,[cs:_mySP]
 ;       mov cs,[_residentCS]
 ;       mov ds,sp
 ;       mov es,ds
 ;       mov ds,ss
-       mov ah,48h       ; move into conventional memory
-       mov bx,[_SwapTransientSize]
-       int 21h
-       jc swaperr
-       mov bx,ax
-       sub bx,[currentSegmOfFreeCOM]
-       push bx
-       mov [currentSegmOfFreeCOM],ax
-       call SWAPXMSdirection
-       mov ah,0bh
-       mov si,_XMSsave
-       callXMS
-       call SWAPXMSdirection
-       pop bx
-       cmp ax,1
-       jnz swaperr
-       mov ax,[_mySS]
-       add ax,bx
-       mov ss,ax
-       mov sp,[_mySP]
-       ; add calling stuff here
+        mov ah,48h       ; move into conventional memory
+        mov bx,[cs:_SwapTransientSize]
+        int 21h
+        jc swaperr
+        mov bx,ax
+        sub bx,[cs:currentSegmOfFreeCOM]
+        push bx
+        mov [cs:currentSegmOfFreeCOM],ax
+        call SWAPXMSdirection
+        mov ah,0bh
+        mov si,_XMSsave
+        callXMS
+;        call SWAPXMSdirection
+        pop bx
+        cmp ax,1
+        jnz swaperr
+;       mov ax,[_mySS]
+;       add ax,bx
+;       mov ss,ax
+;       mov sp,[_mySP]
+%if 0
+        mov ax,[_mySS]
+        sub ax,10
+        mov ss,ax
+        mov sp,[_mySP]
+%endif
+       ; calling stuff here
 ;       push es
-;       push ds
-       push si
+        push ds
+        pop si
+        push si
+        mov ds,sp
 ;       push word 1
 ;       call _process_input
-       call _my2e_parsecommandline
-       push ax
-       mov ah,0bh       ; move into XMS again
-       mov si,_XMSsave
-       callXMS
-       call SWAPXMSdirection
-       pop ax
-       jmp finish
+        call far [myfar2e_parsecommandline]
+%if 0
+        push ax
+        mov ah,0bh       ; move into XMS again
+        mov si,_XMSsave
+        callXMS
+        call SWAPXMSdirection
+        pop ax
+%endif
+        jmp finish
 swaperr:
-       mov ax,0FFFFh
+        mov ax,0FFFFh
 finish:
-;       pop cs
-;       pop ss
-;       pop sp
-       iret
+        iret
