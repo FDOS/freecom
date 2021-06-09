8268f6a03807c835460ceef398c2b94bbd8851ae
Kenneth J Davis <PerditionC@gmail.com>
2021-06-09 11:43:12 -0400

    add some missing languages to string build batch file

--- scripts/makecmd.bat
+++ scripts/makecmd.bat
@@ -8,13 +8,16 @@
 : where no arguments creates all variants
 : where YY Name just creates version for specified language
 if "%1"=="continue" goto loop
+SET MAKEALL=%1
 if "%1"=="" SET MAKEALL=EN
 if "%1"=="" del strings.* > NUL
 set languages=%1 %2
-if "%1"=="" set languages=NL Dutch ES Spanish DE German FR French IT Italian PTR PT_BR RU Russian Ser Serbian TR Turkish YUG YU437 EN English
+if "%1"=="" set languages=NL Dutch ES Spanish DE German FI Finnish FR French IT Italian PO Polish PTR PT_BR RU Russian SE Swedish Ser Serbian SI Slovene TR Turkish UKR Ukr YUG YU437
 call %0  continue %languages%
-set MAKEALL=
+:: do english last and keep the strings.dat
+if "%1"=="" call %0 continue EN English
 set languages=
+set MAKEALL=
 goto end
 
 :loop
@@ -28,15 +31,17 @@ goto end
 :compile
 if "%2"=="" goto end
 for %%x in ( command.cln xmsswap.cln fixstrs.exe default.lng %2.lng ) do if not exist %%x goto end
+if exist strings.err del strings.err
+if exist strings.log del strings.log
 fixstrs %2.lng
 copy strings.dat strings.%1 > NUL
-if "%MAKEALL%"=="" copy /b command.cln + strings.dat command.com > NUL
-if "%MAKEALL%"=="" copy /b xmsswap.cln + strings.dat xmsswap.com > NUL
+if "%MAKEALL%"=="%1" copy /b command.cln + strings.dat command.com > NUL
+if "%MAKEALL%"=="%1" copy /b xmsswap.cln + strings.dat xmsswap.com > NUL
 if exist strings.err echo Translation file out of date for language %2 [file: %2.lng]!
 if exist strings.log echo Translation file out of date for language %2 [file: %2.lng]!
 :: leave strings.dat if matches MAKEALL, which must be last language specified, i.e. leave a default STRINGS.DAT
 if NOT "%MAKEALL%"=="%1" if exist strings.dat del strings.dat
-if "%MAKEALL%"=="%1" if exist strings.log del strings.log
+if NOT "%MAKEALL%"=="%1" if exist strings.log del strings.log
 if exist strings.h   del strings.h
 shift
 goto loop
