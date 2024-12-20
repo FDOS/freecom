f1734e9b1c647230c80cea07ea2003eec7042755
Andrew Bird <ajb@spheresystems.co.uk>
2024-12-20 15:33:02 +0000

    CI: Add Openwatcom 1.9 build on DOS

--- ci_build.sh
+++ ci_build.sh
@@ -25,7 +25,7 @@ LANGUAGES="english dutch finnish french german italian polish pt pt_br russian s
 for lng in ${LANGUAGES} ; do
   # Do full clean for rebuild of each language
   echo "Do full clean"
-  git clean -x -d -f -e _output -e _watcom -e $OWTAR >/dev/null 2>&1
+  git clean -q -x -d -f -e _output -e _watcom -e $OWTAR
   export LNG=${lng}
   ./build.sh gcc
   TGT="_output/gcc/${LNG}"
@@ -37,12 +37,31 @@ done
 for lng in ${LANGUAGES} ; do
   # Do full clean for rebuild of each language
   echo "Do full clean"
-  git clean -x -d -f -e _output -e _watcom -e $OWTAR >/dev/null 2>&1
+  git clean -q -x -d -f -e _output -e _watcom -e $OWTAR
   export LNG=${lng}
   ./build.sh wc
   TGT="_output/wc/${LNG}"
   mkdir -p ${TGT}
   mv -i command.com ${TGT}/.
 done
+
+# Watcom (DOS) (slow so just English)
+mkdir -p _output/wc_dos/english
+git clean -q -x -d -f -e _output -e _watcom -e $OWTAR
+{
+  echo set COMPILER=WATCOM
+  echo set WATCOM='C:\\devel\\watcomc'
+  echo set MAKE=wmake /ms /h /f
+  echo set XNASM='C:\\devel\\nasm\\nasm'
+  echo set XUPX=upx --8086 --best
+  echo set OLDPATH=%PATH%
+  echo set PATH='%WATCOM%\\binw;C:\\bin;%OLDPATH%'
+  echo set DOS4G=QUIET
+} | unix2dos > config.bat
+
+cp config.std config.mak
+
+dosemu -td -q -K . -E "build.bat wc"
+mv -i command.com _output/wc_dos/english/.
 
 echo done
