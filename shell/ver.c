657d57a24272159310ba766bb6d224e25b299896
Kenneth J Davis <PerditionC@gmail.com>
2024-12-22 18:37:48 -0500

    build 0.86 docs update

--- shell/ver.c
+++ shell/ver.c
@@ -41,7 +41,7 @@
 #include "../err_fcts.h"
 #include "../strings.h"
 
-const char shellver[] = "0.85b - "
+const char shellver[] = "0.86 - "
 #if defined(__BORLANDC__)
 	"BORLANDC"
 #elif defined(__TURBOC__)
