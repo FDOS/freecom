158fc0ff0ac1fb40adf3a7d20521d4e99ad2fa0f
Kenneth J Davis <PerditionC@gmail.com>
2024-12-24 14:51:02 -0500

    fix test for Open Watcom
    char *envp[] argument in main is non-standard and is not initialized so displays garbage/crashes test program

--- tests/testenv.c
+++ tests/testenv.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+/* OW does not seem to support *env[] arg to main */
+extern char **environ;
+
 int
 main(int argc, char *argv[], char *env[])
 {
@@ -11,16 +15,16 @@ main(int argc, char *argv[], char *env[])
     printf("%d: %s\n", count, argv[count]);
 
   puts("\nEnvironment:");
-
-  for (count = 0; env[count]; count++)
-    printf("%d: %s\n", count, env[count]);
+  
+  for (count = 0; environ[count] != NULL; count++)
+    printf("%d: %s\n", count, environ[count]);
 
   putenv("PATH=");
 
   puts("\nEnvironment after a setenv (\"PATH=\");:");
 
-  for (count = 0; env[count]; count++)
-    printf("%d: %s\n", count, env[count]);
+  for (count = 0; environ[count]; count++)
+    printf("%d: %s\n", count, environ[count]);
 
   return 0;
 }
