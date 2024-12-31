df83768ed0b58060cf53fc32b565f3c637e988da
Jiri Malak <malak.jiri@gmail.com>
2024-12-31 22:37:28 +0100

    build: fix clean scripts

--- suppl/clnsuppl.bat
+++ suppl/clnsuppl.bat
@@ -1,13 +1,13 @@
 @echo off
 IF EXIST SKIP goto noSrc
 rem for %%a in (compile.me *.h suppl*.lib suppl*.lst vars.bat) do if exist %%a del %%a
-for %%i in (all_done untar.exe untar.com untar.obj untar.map) do if exist %%i del %%i>nul
+for %%i in (all_done untar.exe untar.com untar.obj untar.map gnumake.mak) do if exist %%i del %%i>nul
 for %%i in (compile.me suppl*.lib suppl*.lst echoto.bat vars.bat *.cfg)  do if exist %%i del %%i>nul
 IF NOT EXIST src\NUL goto noSrc
 
 cd src
 FOR %%A in (*.obj) DO DEL %%A
-for %%a in (suppllib.rsp echolib.bat *.cfg) do if exist %%a del %%a>nul
+for %%a in (suppllib.rsp echolib.bat *.cfg gnumake.mak) do if exist %%a del %%a>nul
 rem FOR %%A in (NLS\*.*) DO DEL %%A
 rem RMDIR nls
 CD ..
